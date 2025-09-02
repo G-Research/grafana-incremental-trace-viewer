1814d137eb366c1b92d84a9c8315a2f967db5565
nojaf <florian.verdonck@outlook.com>
2025-09-02 14:57:11 +0200

    Document the current setup

--- tests/panel.spec.ts
+++ tests/panel.spec.ts
@@ -17,19 +17,12 @@ async function getLastTraceId() {
   console.log(start, end);
   const q = '{}';
   const url = `http://localhost:3200/api/search?q=${encodeURIComponent(q)}&start=${start}&end=${end}`;
-  console.log(url);
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
   return data.traces[0].traceID;
 }
 
-test('should read our tempo datasource', async ({
-  panelEditPage,
-  readProvisionedDataSource,
-  page,
-  gotoDashboardPage,
-}) => {
+test('should read our tempo datasource', async ({ page, gotoDashboardPage }) => {
   const traceId = await getLastTraceId();
   await gotoDashboardPage({
     uid: 'gr-trace-viewer-dashboard',
@@ -39,19 +32,4 @@ test('should read our tempo datasource', async ({
   });
 
   await expect(page.getByTestId('data-testid Panel header Incremental Trace Viewer')).toBeVisible();
-  //   await expect(page.getByTestId('simple-panel-circle')).toBeVisible();
 });
-
-// test('should display series counter when "Show series counter" option is enabled', async ({
-//   gotoPanelEditPage,
-//   readProvisionedDashboard,
-//   page,
-// }) => {
-//   const dashboard = await readProvisionedDashboard({ fileName: 'dashboard.json' });
-//   const panelEditPage = await gotoPanelEditPage({ dashboard, id: '1' });
-//   const options = panelEditPage.getCustomOptions('Yozora');
-//   const showSeriesCounter = options.getSwitch('Show series counter');
-
-//   await showSeriesCounter.check();
-//   await expect(page.getByTestId('simple-panel-series-counter')).toBeVisible();
-// });
